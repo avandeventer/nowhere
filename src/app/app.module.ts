833486c1dfc8da979ea24809d31b8faee58a3dcf
Andy Van Deventer <vandeventerandy@gmail.com>
2019-10-17 17:00:35 -0400

    Initial commit bruv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { EncounterComponent } from './encounter/encounter.component';
+import { OptionComponent } from './option/option.component';
+import { LocationComponent } from './location/location.component';
+import { PlayerManagerComponent } from './player-manager/player-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    EncounterComponent,
+    OptionComponent,
+    LocationComponent,
+    PlayerManagerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
